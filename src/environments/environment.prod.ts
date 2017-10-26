f4a6ffabd9a9fafcb1a9e7bdfe58496616511dd9
Sergi Tubert Sabater <stubertsabater@gmail.com>
2017-10-26 19:10:33 +0200

    First commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAlnm4NZtsW6FHzfHlRFcuGJdh8PD_JIqk',
+    authDomain: 'oshop-92852.firebaseapp.com',
+    databaseURL: 'https://oshop-92852.firebaseio.com',
+    projectId: 'oshop-92852',
+    storageBucket: 'oshop-92852.appspot.com',
+    messagingSenderId: '965202716240'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAlnm4NZtsW6FHzfHlRFcuGJdh8PD_JIqk',
+    authDomain: 'oshop-92852.firebaseapp.com',
+    databaseURL: 'https://oshop-92852.firebaseio.com',
+    projectId: 'oshop-92852',
+    storageBucket: 'oshop-92852.appspot.com',
+    messagingSenderId: '965202716240'
+  }
 };
